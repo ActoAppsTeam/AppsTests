0f29636baafa91ebb2a8948fd77eb3d894c09825
ekroes <ekroes@ltekroes.acto.nl>
2018-11-08 17:44:11 +0100

    create new werkorder

--- Scripts/ASMV3/CreateNewRegistratie/Script1541583503707.groovy
+++ Scripts/ASMV3/CreateNewRegistratie/Script1541583503707.groovy
@@ -16,31 +16,61 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.click(findTestObject('ASMV3/Registratie/OpenGlobalMenuNav'))
 
-WebUI.waitForElementPresent(findTestObject('ASMV3/Registratie/GlobalMenuNavButtonCollapsed'), 10)
+WebUI.waitForElementPresent(findTestObject('ASMV3/Registratie/GlobalMenuNavButtonCollapsed'), 5)
 
 WebUI.click(findTestObject('ASMV3/Registratie/ClickNewRegistration'))
 
 WebUI.click(findTestObject('ASMV3/Registratie/Input Objects/button_DossierSearch'))
 
-WebUI.delay(5)
+WebUI.delay(3)
 
 WebUI.setText(findTestObject('ASMV3/Registratie/Input Objects/input_DossierSearchDialog'), DossierNaam)
 
-WebUI.delay(5)
+WebUI.delay(3)
 
 WebUI.click(findTestObject('ASMV3/Registratie/Input Objects/button_DossierSearchDialog_Search'))
 
-WebUI.delay(5)
+WebUI.delay(3)
 
 WebUI.click(findTestObject('ASMV3/Registratie/Input Objects/select_DossierSearchItem'))
 
-WebUI.delay(5)
+WebUI.delay(3)
 
 WebUI.setText(findTestObject('ASMV3/Registratie/Input Objects/input_InstallatieSearchDialog'), InstallatieNaam)
 
-WebUI.delay(5)
+WebUI.delay(3)
 
 WebUI.click(findTestObject('ASMV3/Registratie/Input Objects/button_InstallatieSearchDialog_Search'))
 
-WebUI.delay(5)
+WebUI.delay(3)
+
+WebUI.click(findTestObject('ASMV3/Registratie/Input Objects/select_InstallatieSearchItem'))
+
+WebUI.delay(3)
+
+WebUI.focus(findTestObject('ASMV3/Registratie/Input Objects/input_BezoekSoort'))
+
+WebUI.delay(3)
+
+WebUI.selectOptionByIndex(findTestObject('ASMV3/Registratie/Input Objects/input_BezoekSoort'), BezoekSoort, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.delay(3)
+
+WebUI.setText(findTestObject('ASMV3/Registratie/Input Objects/input_Melder'), Melder)
+
+WebUI.delay(3)
+
+WebUI.setText(findTestObject('ASMV3/Registratie/Input Objects/input_Bedrijf'), Bedrijf)
+
+WebUI.delay(3)
+
+WebUI.setText(findTestObject('ASMV3/Registratie/Input Objects/input_Referentie'), Referentie)
+
+WebUI.setText(findTestObject('ASMV3/Registratie/Input Objects/input_MeldingsTekst'), MeldingsTekst)
+
+WebUI.delay(3)
+
+WebUI.click(findTestObject('ASMV3/Registratie/Input Objects/button_Versturen'))
+
+WebUI.delay(60)
 
